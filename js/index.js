6a583baa7091808d51a2df7c5c131288ed90c950
nanter1986 <nanter1986@gmail.com>
2018-02-16 10:40:59 +0200

    find us button works

--- js/index.js
+++ js/index.js
@@ -32,7 +32,7 @@ $(document).ready(function() {
 
   $("#messageCall").fadeIn(2000, function() {
     $("#messageCall2").fadeIn(2000, function() {
-      $("#buttonCall").fadeIn(2000);
+      $("#buttonCall").css("display","inline-block");
     });
 
   });
