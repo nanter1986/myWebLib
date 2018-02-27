8e4bb61c152c5c3e9ab00b2e0b5afbd3a8a51b42
nanter1986 <nanter1986@gmail.com>
2018-02-27 09:35:56 +0200

    moved all function declarations to functionLib

--- js/functionLib.js
+++ js/functionLib.js
@@ -9,3 +9,79 @@ function createWaypoint(id,timeInMillis){
   });
   return waypoint;
 }
+
+function changeActiveNavBarItem(){
+  $(".nav li").on("click", function() {
+    console.log("clicked on item");
+    $(".nav li").removeClass("active");
+    $(this).addClass("active");
+  });
+}
+
+function closeDropdownAfterClick(){
+  $('.navbar-nav>li>a').on('click', function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+}
+
+
+
+function backToTopAfterPoint(desiredHeight){
+  $(window).scroll(function() {
+    var height = $(window).scrollTop();
+    if (height > desiredHeight) {
+      $('#back2Top').fadeIn();
+    } else {
+      $('#back2Top').fadeOut();
+    }
+  });
+}
+
+
+
+
+
+
+function scrollSlowlyToThePoint(point){
+  $("#back2Top").click(function(event) {
+    event.preventDefault();
+    $("html, body").animate({
+      scrollTop: point
+    }, "slow");
+    $(".nav li").removeClass("active");
+    return false;
+  });
+}
+
+$("#messageCall").fadeIn(2000, function() {
+  $("#messageCall2").fadeIn(2000, function() {
+    $("#buttonCall").css("display","inline-block");
+  });
+
+});
+
+
+
+function zoomTransClassImageToScale(scale){
+  $(".trans").on("mouseenter",function(){
+    console.log("over trans");
+    var value='scale('+scale+')';
+    $(this).css('transform',value);
+  });
+}
+
+
+
+function zoomOutTransClassImageToScale(scale){
+  $(".trans").on("mouseleave",function(){
+    console.log("over trans");
+    var value='scale('+scale+')';
+    $(this).css('transform',value);
+  });
+}
+
+function rotateButtonOnHover(){
+  $("#back2TopImage").on("mouseenter",function(){
+    $(this).css('transform','rotateY(180deg)');
+  });
+}
